3f898aa8fb726c627ad875da5107e7e5a47fcdbe
kuralnithi <kuralnithi1999@gmail.com>
2024-02-06 23:08:15 +0530

    icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,18 +12,18 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <img className="iconimg" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwzzGqfrah7TlqJviJmqH4b7Qdbugqfo2aZI-D6OCGDg&s"} alt="Email icon" />
           <a href="mailto:kuralnithi1999@gmail.com">kuralnithi1999@gmail.com</a>
         </li>
         <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
+          <img className="iconimg"
+            src={"https://cdn-icons-png.flaticon.com/256/174/174857.png"}
             alt="LinkedIn icon"
           />
           <a href="https://www.linkedin.com/in/kural-nithi-0b967122b" target="blank">linkedin.com/kuralnithi</a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <img className="iconimg" src={"https://cdn-icons-png.flaticon.com/512/25/25231.png"} alt="Github icon" />
           <a href="https://www.github.com/kuralnithi" target="blank">github.com/kuralnithi</a>
         </li>
       </ul>
